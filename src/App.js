f409d1aab29fb4ff8922cb4400ef79aa7a06fa9b
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-03-05 17:24:22 -0800

    Single page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
-import Products from './Products.js';
+import styled from 'styled-components';
+
+const Card = styled.button`
+  font-family: 'Lato', sans-serif;
+  border-radius: 3px;
+  padding: 10px;
+  background-color: white;
+  border: 1px solid #ccc;
+  max-width: 300px;
+  margin: 10px;
+`;
+
+const Button = styled.div`
+  border-radius: 3px;
+  padding: 5px;
+  margin: 0 auto;
+  background: transparent;
+  color: black;
+  border: 1px solid #ccc;
+  width: 100px;
+`;
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = { cart: [] }
+    this.state = { cart: [], data: [] }
     this.addToCart = this.addToCart.bind(this);
     this.updateTotal = this.updateTotal.bind(this);
+  }
+
+  getProducts(){
+    fetch('https://raw.githubusercontent.com/Sellbrite/fe-sample-project/master/product-payload.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({data: data.products})
+      })
+      .catch(err => console.err(this.props.url))
+  }
+
+  componentDidMount(){
+    this.getProducts()
   }
 
   addToCart(props){
@@ -32,7 +65,18 @@ class App extends Component {
           <div className="nav-bar-name">Cart.ly</div>
           <div className="nav-bar-cart">cart</div>
         </div>
-        <Products onClick={()=> this.addToCart(this.state.props)} />
+        <div>
+        {this.state.data.map((product) =>{
+          return (
+            <Card key={product.filename}>
+              <img src={"https://raw.githubusercontent.com/Sellbrite/fe-sample-project/master/images/" + product.filename} width="100%"/>
+              <h3>{product.name}</h3>
+              <h1>${(product.price * .01).toFixed(2)}</h1>
+              <Button onClick={()=> this.addToCart(product)}>Add to Cart</Button>
+            </Card>
+          )
+        })}
+      </div>
       </div>
     );
   }
