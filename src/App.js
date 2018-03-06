0e733e66e33ac9f36e73e4cf8b38bd4d580bd1d0
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-03-05 17:43:49 -0800

    Add overlay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
 
+const Overlay = styled.div`
+  background-color: rgba(255, 255, 255, 0.6);
+  z-index: 9;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+`;
+
 const Card = styled.button`
   font-family: 'Lato', sans-serif;
   border-radius: 3px;
@@ -25,9 +33,10 @@ const Button = styled.div`
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = { cart: [], data: [] }
+    this.state = { cart: [], data: [], value: 0 }
     this.addToCart = this.addToCart.bind(this);
     this.updateTotal = this.updateTotal.bind(this);
+    this.showCart = this.showCart.bind(this);
   }
 
   getProducts(){
@@ -43,27 +52,34 @@ class App extends Component {
     this.getProducts()
   }
 
-  addToCart(props){
-    console.log('gettinghere', props);
-    // console.log('product', product);
-    // this.setState((prevState) =>
-    //   { cart: prevState.cart.push(product)});
-    // this.updateTotal();
+  addToCart(product){
+    console.log('product', product);
+    this.setState((prevState) =>
+      { cart: prevState.cart.push(product)});
+    this.updateTotal();
   }
 
   updateTotal(){
     console.log('cart', this.state.cart);
     let itemsInCart = this.state.cart.length;
     console.log(itemsInCart);
+    this.setState({ value: +1 });
+  }
+
+  showCart(){
+    console.log('SHOWING CART!!!');
+    console.log(this.state.cart);
+
   }
 
 
   render() {
     return (
       <div className="App">
+        <Overlay />
         <div className="nav-bar">
           <div className="nav-bar-name">Cart.ly</div>
-          <div className="nav-bar-cart">cart</div>
+          <div className="nav-bar-cart"><span onClick={() => this.showCart()}>My Cart </span> {this.state.value}</div>
         </div>
         <div>
         {this.state.data.map((product) =>{
